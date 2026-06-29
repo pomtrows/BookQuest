9bf1d3977ca492a72658d24dbfb8f955c6a62f06
Pomito <pomito@github.com>
2026-06-29 22:12:41 +0200

    Explicitly register service worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Register the PWA service worker explicitly
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
